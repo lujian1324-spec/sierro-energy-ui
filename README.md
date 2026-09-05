43e15290b400c442fd7198bffad1d1fd7733df45
lujian1324-spec <lujian1324@gmail.com>
2026-09-05 19:00:49 +0800

    pages: CDN PWA full dist

--- README.md
+++
@@ -1,8 +0,0 @@
-# Sierro Energy UI
-
-Pixel-matched Vite React TS UI (402x874).
-
-Routes: /login /devices /devices/empty /device/:id /insights /setting
-
-npm install
-npm run build
--- assets/index-izIHGXua.boot.js
+++
@@ -1,6 +0,0 @@
-const parts=[];
-parts.push(await fetch(new URL("./index-izIHGXua.c0.js",import.meta.url)).then(r=>r.text()));
-parts.push(await fetch(new URL("./index-izIHGXua.c1.js",import.meta.url)).then(r=>r.text()));
-parts.push(await fetch(new URL("./index-izIHGXua.c2.js",import.meta.url)).then(r=>r.text()));
-parts.push(await fetch(new URL("./index-izIHGXua.c3.js",import.meta.url)).then(r=>r.text()));
-await import(URL.createObjectURL(new Blob([parts.join("")],{type:"text/javascript"})));
